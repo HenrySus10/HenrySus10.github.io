4ef0002450f1d2672bf42dbcc7cbb651e74d77e2
Henrysus10 <henrysusantin@gmail.com>
2021-08-09 20:14:41 -0700

    CSS Splitting

--- src/contents/About.js
+++ src/contents/About.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class About extends Component {
     render() {
         return (
-            <div className="condiv">
+            <div className="condiv about">
                 <h1 className="subtopic">About Me</h1>
                 <h4>Hey there,</h4>
                 <h1>My name is Henry Susantin</h1>
