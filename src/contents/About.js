5fd270244148f6c4bf21c0057e6adab331d0b0a4
Henrysus10 <henrysusantin@gmail.com>
2021-08-21 17:14:24 -0700

    About Update

--- src/contents/About.js
+++ src/contents/About.js
@@ -8,7 +8,9 @@ class About extends Component {
                 <h1 className="subtopic">About Me</h1>
                 <br></br>
                 <p>
-                    Hi! My name is Henry Susantin and I'm a current CSE student at UC Irvine. 
+                    Hi! My name is Henry Susantin and I'm a current CSE student at UC Irvine. I'm a problem solver at heart,
+                    with an affinity for computer science and new subjects. In my spare time, I enjoy video games, music mixing,
+                    composition, and playing, as well as making quirky side projects for my own amusement.
                 </p>
             </div>
         )
